edb82440c973bad8565575e6edca416306a3a8cf
Luan <jadsonluan1@gmail.com>
2018-10-29 21:59:23 -0300

    Melhoria nos programas (comentarios e threads)

--- simulado-29-10-2018/exercicio1/client.py
+++ simulado-29-10-2018/exercicio1/client.py
@@ -1,22 +1,35 @@
+# coding: utf-8
 import socket
 from threading import Thread
-HOST = '150.165.42.165'     # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
+
+HOST = '' # Endereço IP do servidor
+PORT = 5000 # Porta do servidor
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
-tcp.connect(dest)
 
+tcp.connect(dest)
+print "Conectado! Envie uma mensagem."
+
+# Recebe mensagens do servidor enquanto houver conexão.
 def read_messages(tcp):
-    while True:
+    try:
         response = tcp.recv(1024)
-        print response
+        while (tcp.fileno() != -1) and response:
+            print "Server>", response
+            response = tcp.recv(1024)
+    except:
+        pass
 
 th1 = Thread(target=read_messages, args=[tcp])
+th1.daemon = True
 th1.start()
 
-print "Conectou"
 msg = raw_input()
-while msg != "bye":
+while True:
     tcp.send (msg)
+    if msg == "bye": break
     msg = raw_input()
+
+print "Encerrando conexão com servidor!"
 tcp.close()
--- simulado-29-10-2018/exercicio1/server.py
+++ simulado-29-10-2018/exercicio1/server.py
@@ -2,33 +2,74 @@
 import socket
 from threading import Thread
 
-HOST = '150.165.42.165'              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
+HOST = '' # Endereço IP do servidor
+PORT = 5000 # Porta do servidor
+num_connections = 5 # número máximo de conexões
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Opção para permitir reuso do endereço de IP após fechamento
 tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 orig = (HOST, PORT)
 tcp.bind(orig)
-tcp.listen(5)
+tcp.listen(num_connections)
+
+# Método que recebe mensagens de determinado cliente e envia a mesma mensagem de
+# volta para todos os clientes conectados ao servidor.
+def read_messages(client, connections):
+    connection = client[0]
+    client_ip = client[1][0]
+
+    # Enquanto houver conexão ...
+    msg = connection.recv(1024)
+    while msg != "bye":
+        for current_client in connections:
+            current_client[0].send(msg)
+        msg = connection.recv(1024)
+
+    print "O cliente", client_ip, "desconectou-se!"
+    print "Atualmente há", len(connections) - 1, "cliente(s) conectados!"
+    print ""
+
+    connection.close()
+    connections.remove(client)
 
 connections = []
 
-def read_messages(connection, connections):
-    while True:
-        con = connection[0]
-        msg = con.recv(1024)
-
-        for current_con in connections:
-            current_con[0].send(str(connection[1][0]) + " diz: " + msg)
-
-        if msg == "bye":
-            con.close()
-            connections.remove(connection)
-
-while True:
-    print "Esperando conexão ..."
+# Aceita uma conexão com o servidor e em seguida cria um thread de tratamento de
+# mensagens para ela.
+def accept_client(tcp, connections):
+    print "Esperando conexão [...]"
     con, client = tcp.accept()
-    print "Conectado por", client
     connections.append((con, client))
-    thread1 = Thread(target=read_messages, args=[(con, client), connections])
-    thread1.start()
-    if (len(connections) == 0): break
+
+    print "Conectado por", client
+    print "Atualmente há", len(connections), "cliente(s) conectados!"
+    print ""
+
+    thread = Thread(target=read_messages, args=[(con, client), connections])
+    thread.daemon = True
+    thread.start()
+
+# Itera a aceitação de conexões
+def loop_accept(tcp, connections):
+    while True:
+        accept_client(tcp, connections)
+
+
+# cliente inicial
+accept_client(tcp, connections)
+
+# Cria e executa daemon/thread para aceitar conexões de forma paralela
+# Importante ser daemon, pois ao finalizar o programa, todos os daemon
+# são finalizados
+client_threads = Thread(target=loop_accept, args=[tcp, connections])
+client_threads.daemon = True
+client_threads.start()
+
+# Para que o programa continue rodando enquanto houver clientes conectados!
+while len(connections) != 0:
+    pass
+
+tcp.close()
